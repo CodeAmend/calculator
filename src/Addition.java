0b1182704b8bb3cb31514ae0a34df57b72ec850c
Michael Bruce Allen <CodeAmend@Gmail.Com>
2015-10-09 09:54:17 -0400

    green : added Addition logic to execute()

--- src/Addition.java
+++ src/Addition.java
@@ -7,5 +7,8 @@ public class Addition implements Operation {
 
     @Override
     public void execute(Stack<Double> stack) {
+        double n2 = stack.pop();
+        double n1 = stack.pop();
+        stack.push(n1 + n2);
     }
 }
