914c28d7ff33024f255d5d458f9963f648ac58f9
Michael Bruce Allen <CodeAmend@Gmail.Com>
2015-10-09 08:52:35 -0400

    green : added Stack object. added input(double)

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,14 +1,20 @@
+import java.util.Stack;
+
 /**
  * Created by codeamend on 10/9/15.
  */
 public class Calculator {
 
+    Stack<Double> stack = new Stack<>();
+
     public double accumulator() {
+        if(stack.size() > 0)
+            return stack.peek();
         return 0;
     }
 
     public void input(double value) {
-
+        stack.push(value);
     }
 
 }
